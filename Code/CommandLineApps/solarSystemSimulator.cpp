0b662d60810057e41aa70441407c1b6690c16874
Leo.Liang <ucapgl0@ucl.ac.uk>
2022-03-14 18:24:58 +0000

    add energy calculation

--- Code/CommandLineApps/solarSystemSimulator.cpp
+++ Code/CommandLineApps/solarSystemSimulator.cpp
@@ -9,31 +9,14 @@
 #include <Eigen/Dense>
 #include <string>
 
-static void show_usage(std::string name)
-{
-	std::cerr << "Usage: " << name << "\n"
-		<< "Users should input the step size and the length of time to simulate the model.\n"
-		<< "Option:\n"
-        << "\t-s\t\tstep size(the time of each step)\n"
-        << "\t-t\t\tthe total time of simulation\n"
-		<< "\t-h,--help\t\tShow this help message\n" 
-		<< std::endl;
-}
 
 
 int main(int argc, char* argv[]) {
 	
-    // if (argc == 1) {
-	// 	show_usage(argv[0]);
-	// 	return 0;
-	// }
-
 	CLI::App app{"Solar-system simulator"};
     double step_size, time_length;
-    app.add_option("-s,--TimeStep", step_size,  "step size(the time of each step)");
-    app.add_option("-t,--LengthOfTime", time_length,  "the total time of simulation");
-    // app.add_option("-h","Users should input the step size and the length of time to simulate the model.");
-    // app.add_option("-help","Users should input the step size and the length of time to simulate the model.");
+    app.add_option("-s,--TimeStep", step_size,  "input the step size(the time of each step)");
+    app.add_option("-t,--LengthOfTime", time_length,  "input the total time of simulation");
     std::string planet[9];
     Eigen::Vector3d position, velocity;
     double mass;
@@ -42,6 +25,9 @@ int main(int argc, char* argv[]) {
     CLI11_PARSE(app, argc, argv);
 
     double Gravitational_constant = 6.67e-11;
+
+    std::clock_t c_start = std::clock();
+    auto t_start = std::chrono::high_resolution_clock::now();
 
     // create a set of MassiveParticle‘s corresponding to the major bodies
     for (int ibody=0; ibody <9; ibody++) {		
@@ -74,10 +60,52 @@ int main(int argc, char* argv[]) {
 		}	
 	}
 
+    // Calculate the kinetic, potential and total energy at the beginning and end of the simulation
+    double k_begin=0, k_end=0, p_begin=0, p_end=0, total_begin, total_end;
+    for (int i=0; i < 9; i++){
+        Eigen::Vector3d ini_velocity = nbsim::solarSystemData[i].velocity;
+        Eigen::Vector3d final_velocity = planet_ptr[i]->getVelocity();
+        k_begin += 0.5 * nbsim::solarSystemData[i].mu * ini_velocity.dot(ini_velocity);
+        k_end += 0.5 * nbsim::solarSystemData[i].mu * final_velocity.dot(final_velocity);
+        Eigen::Vector3d ini_position = nbsim::solarSystemData[i].position;
+        Eigen::Vector3d final_position = planet_ptr[i]->getVelocity();
+        for (int j=0; j<9;j++){
+            if(i!=j){
+                Eigen::Vector3d ini_position_attractor = nbsim::solarSystemData[j].position;
+                Eigen::Vector3d final_position_attractor = planet_ptr[j]->getVelocity();
+                p_begin += -0.5 * nbsim::solarSystemData[i].mu * nbsim::solarSystemData[j].mu / (ini_position - ini_position_attractor).norm();
+                p_end += -0.5 * nbsim::solarSystemData[i].mu * nbsim::solarSystemData[j].mu / (final_position - final_position_attractor).norm();
+            }
+        }
+    }
+
+    std::clock_t c_end = std::clock();
+    auto t_end = std::chrono::high_resolution_clock::now();
+
     // Summarising the position of the solar system bodies at the start and end of the simulation
     std::cout<<"Summarising the position of the solar system bodies at the start and end of the simulation: "<<std::endl;
     for (int i=0;i<9;i++){
 		std::cout<<name[i]<<"\n original position:\n"<<nbsim::solarSystemData[i].position<<"\n current position:\n"<<planet_ptr[i]->getPosition()<<std::endl;
         std::cout<<"\n";
     }
+
+    // Output the energy information at the beginning and end of the simulation
+    total_begin = k_begin + p_begin;
+    total_end = k_end + p_end;
+    std::cout<<"The energy at the beginning of simulation: "<<std::endl;
+    std::cout<<"The Kinetic energy:"<<k_begin<<std::endl;
+    std::cout<<"The Potential energy:"<<p_begin<<std::endl;
+    std::cout<<"The Total energy:"<<total_begin<<std::endl;
+    std::cout<<"The total energy at the end of simulation: "<<std::endl;
+    std::cout<<"The Kinetic energy:"<<k_end<<std::endl;
+    std::cout<<"The Potential energy:"<<p_end<<std::endl;
+    std::cout<<"The Total energy:"<<total_end<<std::endl;
+
+    // Output the runtime of code
+    std::cout << std::fixed << std::setprecision(2) << "CPU time used: "
+              << 1000.0 * (c_end - c_start) / CLOCKS_PER_SEC << " ms\n"
+              << "Wall clock time passed: "
+              << std::chrono::duration<double, std::milli>(t_end-t_start).count()
+              << " ms\n";
+
 }
